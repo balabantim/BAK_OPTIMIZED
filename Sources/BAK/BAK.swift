2d6e1fb002de109622ea5713a5408617efea6a96
ankudinov aleksander <sasha@ankudinov.org.ua>
2023-10-02 14:52:33 +0300

    stability impovment

--- Sources/BAK/BAK.swift
+++ Sources/BAK/BAK.swift
@@ -2,13 +2,12 @@
 //  File.swift
 //  
 //
-//  Created by ankudinov aleksandr on 20.09.2023.
 //
 
 import Foundation
 
 public struct BAK {
-    public private(set) var version = "1.1"
+    public private(set) var version = "1.2"
     public init() {
     }
 }
